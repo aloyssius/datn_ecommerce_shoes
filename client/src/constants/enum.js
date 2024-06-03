6880bc685cbd3ae4232a582868cca1bd1a6eb2bb
aloyssius <tranha.dev@gmail.com>
2024-06-03 22:01:41 +0700

    update enum(OrderTypeOptions)

--- client/src/constants/enum.js
+++ client/src/constants/enum.js
@@ -6,7 +6,6 @@ export const All = {
 export const ProductStatusTabs = {
 }
 
-
 export const Color = {
   INFO: 'info',
   SUCCESS: 'success',
@@ -14,6 +13,34 @@ export const Color = {
   WARNING: 'warning',
   DEFAULT: 'default',
 }
+
+export const OrderTypeOptions = {
+  en: {
+    AT_THE_COUNTER: 'at_the_counter',
+    DELIVERY: 'delivery',
+  },
+  vi: {
+    AT_THE_COUNTER: 'Tại quầy',
+    DELIVERY: 'Giao hàng',
+  }
+};
+
+export const OrderStatusTabs = {
+  en: {
+    PENDING_CONFIRM: 'pending_confirm',
+    WAITTING_DELIVERY: 'watting_delivery',
+    DELIVERING: 'delivering',
+    COMPLETED: 'completed',
+    CANCELED: 'canceled',
+  },
+  vi: {
+    PENDING_CONFIRM: 'Chờ xác nhận',
+    WAITTING_DELIVERY: 'Chờ giao hàng',
+    DELIVERING: 'Đang giao hàng',
+    COMPLETED: 'Hoàn thành',
+    CANCELED: 'Đã hủy',
+  }
+};
 
 export const VoucherStatusTabs = {
   en: {
