2fd2162363012bc5d336f55da7243d3aa5506171
aloyssius <tranha.dev@gmail.com>
2024-06-07 22:16:07 +0700

    update routes

--- server/routes/api.php
+++ server/routes/api.php
@@ -1,5 +1,7 @@
 <?php
 
+use App\Http\Controllers\Api\Bills\BillController;
+use App\Http\Controllers\Api\Vouchers\VoucherController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -17,3 +19,6 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+Route::apiResource('/vouchers', VoucherController::class);
+Route::apiResource('/bills', BillController::class);
