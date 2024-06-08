55658b3b22b4fdbd21e158284e09719a7bc3f144
aloyssius <tranha.dev@gmail.com>
2024-06-08 23:49:13 +0700

    update server

--- server/app/Http/Controllers/Api/Bills/BillController.php
+++ server/app/Http/Controllers/Api/Bills/BillController.php
@@ -3,18 +3,48 @@
 namespace App\Http\Controllers\Api\Bills;
 
 use App\Helpers\ApiResponse;
+use App\Helpers\QueryHelper;
 use App\Http\Controllers\Controller;
 use App\Http\Requests\BillRequest;
 use App\Http\Resources\Bills\BillResource;
 use App\Models\Bill;
+use Carbon\Carbon;
+use Illuminate\Support\Facades\DB;
 
 class BillController extends Controller
 {
     public function index(BillRequest $req)
     {
-        $bills = Bill::select(BillResource::fields())
-            ->orderBy('created_at', 'desc')
-            ->paginate($req->pageSize, ['*'], 'page', $req->currentPage);
+        DB::enableQueryLog();
+
+        $bills = Bill::select(BillResource::fields());
+
+        if ($req->filled('search')) {
+            $search = $req->search;
+            $searchFields = ['code', 'full_name', 'phone_number'];
+            QueryHelper::buildQuerySearchContains($bills, $search, $searchFields);
+        }
+
+        if ($req->filled('status')) {
+            QueryHelper::buildQueryEquals($bills, 'status', $req->status);
+        }
+
+        $bills->when($req->filled('startDate') && $req->filled('endDate'), function ($query) use ($req) {
+            $startDate = Carbon::parse($req->startDate)->startOfDay();
+            $endDate = Carbon::parse($req->endDate)->endOfDay();
+            return $query->whereBetween('created_at', [$startDate, $endDate]);
+        })
+            ->when($req->filled('startDate') && !$req->filled('endDate'), function ($query) use ($req) {
+                $startDate = Carbon::parse($req->startDate)->startOfDay();
+                $query->where('created_at', '>=', $startDate);
+            })
+            ->when(!$req->filled('startDate') && $req->filled('endDate'), function ($query) use ($req) {
+                $endDate = Carbon::parse($req->endDate)->endOfDay();
+                $query->where('created_at', '<=', $endDate);
+            });
+
+        QueryHelper::buildOrderBy($bills, 'created_at', 'desc');
+        $bills = QueryHelper::buildPagination($bills, $req);
 
         return ApiResponse::responsePage(BillResource::collection($bills));
     }
