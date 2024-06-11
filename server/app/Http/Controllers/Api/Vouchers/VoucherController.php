15490224314b604aefeb1a475275d7b4c6e9e312
duongdhph21621@fpt.edu.vn <128455237+duongdhph21621@users.noreply.github.com>
2024-06-11 15:04:16 +0700

    api voucher

--- server/app/Http/Controllers/Api/Vouchers/VoucherController.php
+++ server/app/Http/Controllers/Api/Vouchers/VoucherController.php
@@ -6,12 +6,17 @@ use App\Helpers\ApiResponse;
 use App\Http\Controllers\Controller;
 use App\Http\Resources\Vouchers\VoucherResource;
 use App\Models\Voucher;
-use Illuminate\Http\Request;
+use Illuminate\Http\Request\Page;
+use App\Http\Requests\VoucherRequest;
 
 class VoucherController extends Controller
 {
-    public function index()
+    public function index(VoucherRequest $req)
     {
-        return ApiResponse::responseObject(VoucherResource::collection(Voucher::all()));
+        $vouchers = Voucher::select(VoucherResource::fields())
+            ->orderBy('created_at', 'desc')
+            ->paginate($req->pageSize, ['*'], 'page', $req->currentPage);
+
+        return ApiResponse::responseObject(VoucherResource::collection($vouchers));
     }
 }
