932a6ba6d652599e4a31328c32198e8f26f599ad
aloyssius <tranha.dev@gmail.com>
2024-07-17 21:27:37 +0700

    update api

--- client/src/api/apiConfig.js
+++ client/src/api/apiConfig.js
@@ -15,9 +15,43 @@ export const ADMIN_API = {
 
   product: {
     all: path(ROOTS_API, '/products'),
-    // post: path(ROOTS_API, '/bills'),
-    // put: (id) => path(ROOTS_API, `/bills/${id}`),
-    // details: (id) => path(ROOTS_API, `/bills/${id}`),
+    post: path(ROOTS_API, '/products'),
+    attributes: path(ROOTS_API, '/products/attributes/all'),
+    postAttributes: path(ROOTS_API, '/products/attributes'),
+    put: path(ROOTS_API, `/products`),
+    details: (id) => path(ROOTS_API, `/products/${id}`),
+  },
+
+  color: {
+    all: path(ROOTS_API, '/attributes/colors'),
+    post: path(ROOTS_API, '/attributes/colors'),
+    put: path(ROOTS_API, `/attributes/colors`),
+    delete: path(ROOTS_API, `/attributes/colors`),
+    putStatus: path(ROOTS_API, `/attributes/colors/status`),
+  },
+
+  category: {
+    all: path(ROOTS_API, '/attributes/categories'),
+    post: path(ROOTS_API, '/attributes/categories'),
+    put: path(ROOTS_API, `/attributes/categories`),
+    delete: path(ROOTS_API, `/attributes/categories`),
+    putStatus: path(ROOTS_API, `/attributes/categories/status`),
+  },
+
+  size: {
+    all: path(ROOTS_API, '/attributes/sizes'),
+    post: path(ROOTS_API, '/attributes/sizes'),
+    put: path(ROOTS_API, `/attributes/sizes`),
+    delete: path(ROOTS_API, `/attributes/sizes`),
+    putStatus: path(ROOTS_API, `/attributes/sizes/status`),
+  },
+
+  brand: {
+    all: path(ROOTS_API, '/attributes/brands'),
+    post: path(ROOTS_API, '/attributes/brands'),
+    put: path(ROOTS_API, `/attributes/brands`),
+    delete: path(ROOTS_API, `/attributes/brands`),
+    putStatus: path(ROOTS_API, `/attributes/brands/status`),
   },
 
   promotion: {
