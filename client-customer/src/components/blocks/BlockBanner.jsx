407734f4e6716896f12b30db19c0501ec82c4dc0
aloyssius <tranha.dev@gmail.com>
2024-08-07 22:21:19 +0700

    update components block

--- client-customer/src/components/blocks/BlockBanner.jsx
+++ client-customer/src/components/blocks/BlockBanner.jsx
@@ -6,31 +6,20 @@ import { Link } from 'react-router-dom';
 
 
 export default function BlockBanner() {
-    return (
-        <div className="block block-banner">
-            <div className="container">
-                <Link to="/" className="block-banner__body">
-                    <div
-                        className="block-banner__image block-banner__image--desktop"
-                        style={{ backgroundImage: 'url("images/banners/banner-1.jpg")' }}
-                    />
-                    <div
-                        className="block-banner__image block-banner__image--mobile"
-                        style={{ backgroundImage: 'url("images/banners/banner-1-mobile.jpg")' }}
-                    />
-                    <div className="block-banner__title">
-                        Hundreds
-                        <br className="block-banner__mobile-br" />
-                        Hand Tools
-                    </div>
-                    <div className="block-banner__text">
-                        Hammers, Chisels, Universal Pliers, Nippers, Jigsaws, Saws
-                    </div>
-                    <div className="block-banner__button">
-                        <span className="btn btn-sm btn-primary">Shop Now</span>
-                    </div>
-                </Link>
-            </div>
-        </div>
-    );
+  return (
+    <div className="block block-banner">
+      <div className="container">
+        <Link to="/" className="block-banner__body">
+          <div
+            className="block-banner__image block-banner__image--desktop"
+            style={{ backgroundImage: 'url("https://ananas.vn/wp-content/uploads/Web1920-1.jpeg")' }}
+          />
+          <div
+            className="block-banner__image block-banner__image--mobile"
+            style={{ backgroundImage: 'url("https://ananas.vn/wp-content/uploads/Web1920-1.jpeg")' }}
+          />
+        </Link>
+      </div>
+    </div>
+  );
 }
--- client-customer/src/components/blocks/BlockFeatures.jsx
+++ client-customer/src/components/blocks/BlockFeatures.jsx
@@ -6,59 +6,59 @@ import PropTypes from 'prop-types';
 
 // application
 import {
-    ManuFactureSvg,
-    LightBulbSvg,
-    CalendarSvg,
+  FiFreeDelivery48Svg,
+  FiPaymentSecurity48Svg,
+  FiTag48Svg,
+
 } from '../../svg';
 
-
 export default function BlockFeatures(props) {
-    const { layout } = props;
+  const { layout } = props;
 
-    return (
-        <div className={`block block-features block-features--layout--${layout}`}>
-            <div className="container">
-                <div className="block-features__list">
-                    <div className="block-features__item">
-                        <div className="block-features__icon">
-                            <ManuFactureSvg />
-                        </div>
-                        <div className="block-features__content">
-                            <div className="block-features__title">Fournisseurs</div>
-                            <div className="block-features__subtitle">Découvrez notre annuaire complet de tous les fournisseurs du batiment.</div>
-                        </div>
-                    </div>
-                    <div className="block-features__divider" />
-                    <div className="block-features__item">
-                        <div className="block-features__icon">
-                            <LightBulbSvg />
-                        </div>
-                        <div className="block-features__content">
-                            <div className="block-features__title">Inspiration</div>
-                            <div className="block-features__subtitle">Consultez notre rubrique inspiration pour plus d'idées déco.</div>
-                        </div>
-                    </div>
-                    <div className="block-features__divider" />
-                    <div className="block-features__item">
-                        <div className="block-features__icon">
-                            <CalendarSvg />
-                        </div>
-                        <div className="block-features__content">
-                            <div className="block-features__title">Événement</div>
-                            <div className="block-features__subtitle">Foires, évènements, lancement de produits restez connectés.</div>
-                        </div>
-                    </div>
-               
-                </div>
+  return (
+    <div className={`block block-features block-features--layout--${layout}`}>
+      <div className="container">
+        <div className="block-features__list">
+          <div className="block-features__item">
+            <div className="block-features__icon">
+              <FiFreeDelivery48Svg />
             </div>
+            <div className="block-features__content">
+              <div className="block-features__title">Miễn phí vận chuyển</div>
+              <div className="block-features__subtitle">Cho đơn hàng từ 1,000,000 trở đi</div>
+            </div>
+          </div>
+          <div className="block-features__divider" />
+          <div className="block-features__item">
+            <div className="block-features__icon">
+              <FiTag48Svg />
+            </div>
+            <div className="block-features__content">
+              <div className="block-features__title">Ưu đãi hấp dẫn</div>
+              <div className="block-features__subtitle">Được tặng voucher lên đến 90%</div>
+            </div>
+          </div>
+          <div className="block-features__divider" />
+          <div className="block-features__item">
+            <div className="block-features__icon">
+              <FiPaymentSecurity48Svg />
+            </div>
+            <div className="block-features__content">
+              <div className="block-features__title">Sản phẩm chính hãng</div>
+              <div className="block-features__subtitle">Cam kết 100% hàng chính hãng</div>
+            </div>
+          </div>
+
         </div>
-    );
+      </div>
+    </div>
+  );
 }
 
 BlockFeatures.propTypes = {
-    layout: PropTypes.oneOf(['classic', 'boxed']),
+  layout: PropTypes.oneOf(['classic', 'boxed']),
 };
 
 BlockFeatures.defaultProps = {
-    layout: 'classic',
+  layout: 'classic',
 };
