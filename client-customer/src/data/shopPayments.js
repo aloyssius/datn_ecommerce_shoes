2e60d98e5bfb2e0d1638e55e88514a604056d3fb
aloyssius <tranha.dev@gmail.com>
2024-08-07 22:23:49 +0700

    update data static

--- client-customer/src/data/shopPayments.js
+++ client-customer/src/data/shopPayments.js
@@ -1,23 +1,28 @@
 export default [
-    {
-        key: 'bank',
-        title: 'Direct bank transfer',
-        description: 'Make your payment directly into our bank account. Please use your Order ID as the payment reference. '
-            + 'Your order will not be shipped until the funds have cleared in our account.',
-    },
-    {
-        key: 'check',
-        title: 'Check payments',
-        description: 'Please send a check to Store Name, Store Street, Store Town, Store State County, Store Postcode.',
-    },
-    {
-        key: 'cash',
-        title: 'Cash on delivery',
-        description: 'Pay with cash upon delivery.',
-    },
-    {
-        key: 'paypal',
-        title: 'PayPal',
-        description: 'Pay via PayPal; you can pay with your credit card if you don’t have a PayPal account.',
-    },
+  // {
+  //     key: 'bank',
+  //     title: 'Direct bank transfer',
+  //     description: 'Make your payment directly into our bank account. Please use your Order ID as the payment reference. '
+  //         + 'Your order will not be shipped until the funds have cleared in our account.',
+  // },
+  // {
+  //     key: 'check',
+  //     title: 'Check payments',
+  //     description: 'Please send a check to Store Name, Store Street, Store Town, Store State County, Store Postcode.',
+  // },
+  {
+    key: 'cash',
+    title: 'Thanh toán khi nhận hàng (COD)',
+    description: 'Là phương thức thanh toán bằng tiền mặt trực tiếp khi nhận hàng',
+  },
+  {
+    key: 'vnpay',
+    title: 'Thanh toán qua ví VNPAY',
+    description: 'Phương thức thanh toán trực tiếp qua nền tảng ví điện tử VNPAY',
+  },
+  // {
+  //     key: 'paypal',
+  //     title: 'PayPal',
+  //     description: 'Pay via PayPal; you can pay with your credit card if you don’t have a PayPal account.',
+  // },
 ];
